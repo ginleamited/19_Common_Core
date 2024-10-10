879772982c97e641468d851ca203ca8a2d585c1d
ginleamited <lin.jingyang@hotmail.com>
2024-10-10 18:02:43 +0200

    library

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,18 +6,29 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:45:03 by jilin             #+#    #+#             */
-/*   Updated: 2024/10/09 23:40:44 by jilin            ###   ########.fr       */
+/*   Updated: 2024/10/10 17:18:10 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 void *ft_memset(void *b, int c, size_t len)
 {
-    unsigned char *tmp;
+    int i;
 
-    *tmp = *b;
-    while(len[c])
+    i = 0;
+    while(i < len)
     {
-        
+        ((unsigned char *)b)[i] = c;
+        i++;
     }
+    return(b);
+}
 
+int main(void)
+{
+    int c = 'a';
+    unsigned char b[] = "salut";
+
+    printf("%s", ft_memset(b, c, 2));
 }
