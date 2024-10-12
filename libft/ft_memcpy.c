2e2809b8d585ec79c4e577b575a83c8e318afbb0
ginleamited <lin.jingyang@hotmail.com>
2024-10-12 22:22:18 +0200

    Add

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,24 +6,39 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 20:40:42 by jilin             #+#    #+#             */
-/*   Updated: 2024/10/12 20:40:46 by jilin            ###   ########.fr       */
+/*   Updated: 2024/10/12 21:56:38 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_memcpy(void *dst, const void *src, size_t n)
+void *ft_memcpy(void *dst, const void *src, size_t n)
 {
-    if (!dst && !src)
-        return NULL;
+    unsigned char *tmp_src;
+    unsigned char *tmp_dst;
+    
+    if (dst == 0 || src == 0)
+        return (dst);
 
-     (unsigned char *)tmp_src = (unsigned char *)src;
-     (unsigned char *)tmp_dst = (unsigned char *)dst;
+     tmp_src = (unsigned char *)src;
+     tmp_dst = (unsigned char *)dst;
     
     while(n > 0)
     {
-        *tmp_dst++ = *tmp_src++;
+        *(tmp_dst++) = *(tmp_src++);
         n--;
     }
-    return dst;
+    return (dst);
+}
+
+int main(void)
+{
+    char src[] = "source";
+    char dst[20];
+    
+    ft_memcpy(dst, src, 2);
+    
+    dst[2] = '\0';
+    
+    printf("Destination: %s\n", dst);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,9 +6,11 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 18:03:52 by jilin             #+#    #+#             */
-/*   Updated: 2024/10/10 18:03:57 by jilin            ###   ########.fr       */
+/*   Updated: 2024/10/12 21:47:45 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
