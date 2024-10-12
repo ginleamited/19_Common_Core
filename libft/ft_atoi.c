8a11967d4e4685853f853a51e5358d2f130eccfa
ginleamited <lin.jingyang@hotmail.com>
2024-10-12 20:25:50 +0200

    atoi

---
+++ libft/ft_atoi.c
@@ -0,0 +1,26 @@
+```c
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	signe;
+	int	resultat;
+
+	i = 0;
+	signe = 1;
+	resultat = 0;
+	while ((str[i] == 32) || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == 43 || str[i] == 45)
+	{
+		if (str[i] == 45)
+			signe *= -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		resultat *= 10;
+		resultat += str[i] - 48;
+		i++;
+	}
+	return (resultat * signe);
+}
