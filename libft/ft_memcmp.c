431297ff022a5f07d446a2cd0eedcbe581ebbc48
ginleamited <lin.jingyang@hotmail.com>
2024-10-29 14:49:52 +0100

    add

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 19:41:43 by jilin             #+#    #+#             */
-/*   Updated: 2024/10/28 18:33:37 by jilin            ###   ########.fr       */
+/*   Updated: 2024/10/28 18:37:13 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,3 +35,18 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 // EXPLANATION:
 // Compare the memory value of two strings
+
+// DEBUGGING:
+// Removed the (tmp_s1[i] && tmp_s2[i]) condition to ensure 
+// that the comparison does not stop at a null character.
+// just like in the standard memcmp
+
+// The loop now only checks if i < n and stops as soon 
+// as tmp_s1[i] differs from tmp_s2[i].
+
+// If i reaches n, this means all n bytes are identical, 
+// so the function returns 0.
+
+// If a difference is found before reaching n, 
+// it returns the difference between tmp_s1[i] and tmp_s2[i], 
+// as expected in memcmp.
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,13 +6,18 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 01:29:37 by jilin             #+#    #+#             */
-/*   Updated: 2024/10/28 18:33:55 by jilin            ###   ########.fr       */
+/*   Updated: 2024/10/28 22:09:12 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *str, char c)
+static int	ft_wordcount(const char *str, char c);
+static void	ft_strcpy(char *dst, const char *src, char c, int j);
+static char	*ft_stralloc(const char *str, char c, int *k);
+static void	ft_free(char **strs);
+
+char	**ft_split(const char *str, char c)
 {
 	char	**strs;
 	int		i;
@@ -23,24 +28,25 @@ char	**ft_split(char const *str, char c)
 		return (NULL);
 	i = 0;
 	pos = 0;
-	j = ft_wordcount((char *)str, c);
+	j = ft_wordcount(str, c);
 	strs = (char **)malloc(sizeof(char *) * (j + 1));
 	if (strs == NULL)
 		return (NULL);
 	strs[j] = NULL;
 	while (i < j)
 	{
-		strs[i] = ft_stralloc(((char *)str), c, &pos);
+		strs[i] = ft_stralloc(str, c, &pos);
 		if (strs[i] == NULL)
 		{
-			ft_freeup(strs[i]);
+			ft_free(strs);
+			return (NULL);
 		}
 		i++;
 	}
 	return (strs);
 }
 
-static int	ft_wordcount(char *str, char c)
+static int	ft_wordcount(const char *str, char c)
 {
 	int	i;
 	int	word;
@@ -54,54 +60,55 @@ static int	ft_wordcount(char *str, char c)
 			word++;
 			while (str[i] != c && str[i] != '\0')
 				i++;
-			if (str[i] == '\0')
-				return (word);
 		}
-		i++;
+		else
+			i++;
 	}
 	return (word);
 }
 
-static void	ft_strcpy(char *word, char *str, char c, int j)
+static void	ft_strcpy(char *dst, const char *src, char c, int src_j)
 {
 	int	i;
 
 	i = 0;
-	while (str[j] != '\0' && str[j] == c)
-		j++;
-	while (str[j + i] != c && str[j + i] != '\0')
+	while (src[src_j] != '\0' && src[src_j] == c)
+		src_j++;
+	while (src[src_j + i] != c && src[src_j + i] != '\0')
 	{
-		word[i] = str[j + i];
+		dst[i] = src[src_j + i];
 		i++;
 	}
-	word[i] = '\0';
+	dst[i] = '\0';
 }
 
-static char	*ft_stralloc(char *str, char c, int *k)
+static char	*ft_stralloc(const char *src, char c, int *k)
 {
-	char	*word;
-	int		j;
+	char	*dst;
+	int		src_j;
+	int		start;
 
-	j = *k;
-	word = NULL;
-	while (str[*k] != '\0')
+	dst = NULL;
+	src_j = *k;
+	while (src[*k] != '\0')
 	{
-		if (str[*k] != c)
+		if (src[*k] != c)
 		{
-			while (str[*k] != '\0' && str[*k] != c)
-				*k += 1;
-			word = (char *)malloc(sizeof(char) * (*k + 1));
-			if (word == NULL)
+			start = *k;
+			while (src[*k] != '\0' && src[*k] != c)
+				(*k)++;
+			dst = (char *)malloc(sizeof(char) * (*k - start + 1));
+			if (dst == NULL)
 				return (NULL);
-			break ;
+			ft_strcpy(dst, src, c, start);
+			return (dst);
 		}
-		*k += 1;
+		(*k)++;
 	}
-	ft_strcpy(word, str, c, j);
-	return (word);
+	return (NULL);
 }
 
-static void	*ft_free(char *strs)
+static void	ft_free(char **strs)
 {
 	int	i;
 
@@ -112,7 +119,68 @@ static void	*ft_free(char *strs)
 		i++;
 	}
 	free(strs);
-	return (NULL);
 }
+// int main(void)
+// {
+// 	char **result;
+// 	char str[] = "HELLO WORLD HELLO YOU";
+// 	char delimiter = ' ';
+// 	int i = 0;
 
-// EXPLANATION:
+// 	result = ft_split(str, delimiter);
+	
+// 	if (result != NULL)
+// 	{
+// 		while (result[i] != NULL)
+// 		{
+// 			printf("Word %d: %s\n", i, result[i]);
+// 			free(result[i]);
+// 			i++;
+// 		}
+// 		free(result);
+// 	}
+// 	else
+// 		printf("Error: ft_split returned NULL\n");
+// }
+
+//* EXPLANATION:
+
+//? FT_SPLIT
+// char **strs 	: A pointer to an array of strings (the result).
+// int 	i 		: An index for looping through words.
+// int 	j 		: The number of words found in the string.
+// int 	pos 	: This will hold the current position in the string.
+// First it allocates enough memory for the array of string pointers
+// If allocation failed, then return (NULL).
+
+// Then it will allocate memory for each word and store it in the strs[i].
+// If strs[i] is NUL -> allocation failed -> free -> return NULL
+
+//? FT_WORDCOUNT
+// To count the words
+// int 	i 		: For iteration
+// int 	word 	: Counting words
+// First loop the string, if str[i] != delimiter then add a word, 
+// then loop the string until finishing the word and occuring a 
+// delimiter or the NULL, then getting out from the inner loop 
+// and go in the else condition, then go back in the inner loop again
+// return the number of words in the end
+
+//? FT_STRCPY
+// To copy the word from a string to a newly allocated string
+// char *dst 		: destination where the word will be copied
+// const char *src 	: source string
+// int j 			: index in the source string
+// int i			: index in the destination string
+// First let's loop with index j to skip the leading delimiters
+// Then we will find the first letter or reach the NULL
+
+// The second loop will copy the characters from the src[j + i] to dst[i]
+// until it occurs c or NULL, then NULL terminate the word[i] at the end
+
+//? FT_STRALLOC
+// 
+
+//? INT MAIN
+// char **result : A pointer to an array of strings 
+// (char pointers) that will hold the split words.
