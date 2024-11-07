3db24fd948352a8477b4683a785602e229949aff
ginleamited <lin.jingyang@hotmail.com>
2024-11-07 01:42:16 +0100

    ADD

--- ft_printf/functions_hex.c
+++ ft_printf/functions_hex.c
@@ -6,7 +6,7 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 22:23:02 by jilin             #+#    #+#             */
-/*   Updated: 2024/11/07 01:35:22 by jilin            ###   ########.fr       */
+/*   Updated: 2024/11/07 01:41:30 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,8 @@ int	ft_putpointerhex(unsigned long nb, int flag)
 	base = "0123456789abcdef";
 	if (flag == 1)
 		base = "0123456789ABCDEF";
+	if (nb == 0)
+		count += ft_putchar('0');
 	if (nb >= 16)
 		count += ft_putpointerhex((nb / 16), flag);
 	count += ft_putchar(base[nb % 16]);
