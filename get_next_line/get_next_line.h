426d331f834aa9163eac14b2aca90ce4f81643d6
ginleamited <lin.jingyang@hotmail.com>
2024-11-18 23:32:32 +0100

    ADD

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -5,51 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/11 20:29:17 by jilin             #+#    #+#             */
-/*   Updated: 2024/11/15 14:23:54 by jilin            ###   ########.fr       */
+/*   Created: 2024/11/14 22:43:14 by jilin             #+#    #+#             */
+/*   Updated: 2024/11/18 20:32:13 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef get_next_line_H
-# define get_next_line_H
-
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 5
 # endif
-
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
-typedef struct s_list
-{
-	char			*str_buf;//point to the buffer holding char read from file
-	struct s_list	*next;//point to the next node of the list
-}	t_list;
 
-typedef struct l_stax
-{
-	t_list	*top;
-}	l_stack;
-
-void	sort(l_stack *top)
-{
-	t_list current_top = top->next;
-
-	current_top node 1
-}
-// str_buf: pointer to a dynamically allocated string (buffer)
-// containing a chunk of text read from the file.
-// ext: This is a pointer to the next node in the list, 
-// allowing each node to be linked to the next.
-
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(char *src);
+size_t	ft_strlen(const char *str);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
 char	*get_next_line(int fd);
-void	create_list(t_list **list, int fd);
-void	append(t_list **list, char *buf);
-char	*get_line(t_list *list);
-void	polish_list(t_list **list);
-int		len_to_newline(t_list *list);
-void	copy_str(t_list *list, char *str);
-void	dealloc(t_list **list, t_list *clean_node, char *buf);
 
-#endif
+#endif	
