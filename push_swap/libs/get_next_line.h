b3acc817e7b4ee01a3236f5f571d240f737035fc
Jingyang Lin <jilin@mi-c2-p3.s19.be>
2024-12-10 20:33:53 +0100

    ADD

--- push_swap/libs/get_next_line.h
+++ push_swap/libs/get_next_line.h
@@ -1,33 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/07 14:16:13 by jilin             #+#    #+#             */
-/*   Updated: 2024/12/10 17:30:12 by jilin            ###   ########.fr       */
+/*   Created: 2024/11/14 22:43:14 by jilin             #+#    #+#             */
+/*   Updated: 2024/11/20 12:48:15 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+# include <fcntl.h>
 # include <unistd.h>
 # include <stdio.h>
-# include <stddef.h>
 # include <stdlib.h>
-# include <string.h>
-# include <ctype.h>
-# include "include/libft.h"
-# include "include/printf.h"
 
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(char *src);
+size_t	ft_strlen(const char *str);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
 
-
-
-#endif
+#endif	
