1b0f55d0bfaf7f54abb78dd0a2eefdca00a02b86
Jingyang Lin <jilin@fu-r7-p4.s19.be>
2025-01-14 00:14:56 +0100

    a

--- push_swap/src/push_swap/push_swap.c
+++ push_swap/src/push_swap/push_swap.c
@@ -6,45 +6,34 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 14:16:16 by jilin             #+#    #+#             */
-/*   Updated: 2025/01/13 00:13:11 by jilin            ###   ########.fr       */
+/*   Updated: 2025/01/13 23:38:48 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-// int	main(int ac, char **av)
-// {
-// 	t_stack_node	*a;
-// 	t_stack_node	*b;
-	
-// 	a = NULL;
-// 	b = NULL;
-// 	if (ac == 1 || (ac == 2 && !av[1][0]))
-// 		return (1);
-// 	else if (ac == 2)
-// 		av = split(av[1], ' ');
-// 	init_stack_a(&a, av + 1);
-// 	if (!stack_sorted(a))
-// 	{
-// 		if (stack_len(a) == 2)
-// 			sa(&a, false);
-// 		else if (stack_len(a) == 3)
-// 			sort_three(&a);
-// 		else	sort_stacks(&a, &b);
-// 	}
-// 	free_stack(&a);
-// 	return (0);
-// }
-
-int	main(void)
+int	main(int ac, char **av)
 {
-	char	src[];
-	char	dst[20];
-
-	src[] = "source";
-	ft_memcpy(dst, src, 2);
-	dst[2] = '\0';
-	printf("Destination: %s\n", dst);
+	t_stack_node	*a;
+	t_stack_node	*b;
+	
+	a = NULL;
+	b = NULL;
+	if (ac == 1 || (ac == 2 && !av[1][0]))
+		return (1);
+	else if (ac == 2)
+		av = split(av[1], ' ');
+	init_stack_a(&a, av + 1);
+	if (!stack_sorted(a))
+	{
+		if (stack_len(a) == 2)
+			sa(&a, false);
+		else if (stack_len(a) == 3)
+			sort_three(&a);
+		else	sort_stacks(&a, &b);
+	}
+	free_stack(&a);
+	return (0);
 }
 
 //* EXPLANATION
--- push_swap/src/push_swap/stack_init.c
+++ push_swap/src/push_swap/stack_init.c
@@ -6,11 +6,37 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 19:27:26 by jilin             #+#    #+#             */
-/*   Updated: 2024/12/20 21:05:03 by jilin            ###   ########.fr       */
+/*   Updated: 2025/01/14 00:14:13 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+long	ft_atoi(const char *str)
+{
+	int		i;
+	int		signe;
+	long	resultat;
+
+	i = 0;
+	signe = 1;
+	resultat = 0;
+	while ((str[i] == 32) || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == 45)
+			signe *= -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		resultat *= 10;
+		resultat += str[i] - 48;
+		i++;
+	}
+	return (resultat * signe);
+}
 
 static void	append_node(t_stack_node **stack, int n)
 {
@@ -24,6 +50,7 @@ static void	append_node(t_stack_node **stack, int n)
 		return ;
 	node->next = NULL;
 	node->nbr = n;
+	node->cheapest = 0;
 	if (!(*stack))
 	{
 		*stack = node;
@@ -50,7 +77,28 @@ void	init_stack_a(t_stack_node **a, char **argv)
 		n = ft_atoi(argv[i]);
 		if (n > INT_MAX || n < INT_MIN)
 			free_error(a);
+		if (error_duplicate(*a, (int)n))
+			free_error(a);
 		append_node(a, (int)n);
 		i++;
 	}
 }
+
+t_stack_node	*get_cheapest(t_stack_node *stack)
+{
+	if (!stack)
+		return (NULL);
+	while (stack)
+	{
+		if (stack->cheapest)
+			return (stack);
+		stack = stack->next;
+	}
+	return (NULL);
+}
+void	prep_for_push(t_stack_node **stack, t_stack_node *top_node, char stack_name)
+{
+	while (*stack != top_node)
+	{
+		
+	}
