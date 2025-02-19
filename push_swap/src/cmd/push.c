84d5a8af30e0b7260f02570825a2ef89357ad25e
Jingyang Lin <ginleamited@Jins-BigMac.local>
2025-02-19 19:24:13 +0100

    ADD

---
+++ push_swap/src/cmd/push.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 20:55:20 by jilin             #+#    #+#             */
+/*   Updated: 2025/02/19 19:11:39 by jilin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/push_swap.h"
+
+static void push(t_stack **dst, t_stack **src)
+{
+	t_stack_node	*top_node;
+	
+	if (!*src)
+		return ;
+	top_node = *src;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = NULL;
+	top_node->prev = NULL;
+	if (!*dst)
+	{
+		*dst = top_node;
+		top_node->next = NULL;
+	}
+	else
+	{
+		top_node->next = *dst;
+		top_node->next->prev = top_node;
+		*dst = top_node;
+	}
+}
+//* EXPLANATION:
+//?	This function is pushing the top element of src to the top of dst
+//  the top_node will store the top element of src
+//  src will point to the next element, then we will pop the top element
+//  If dst is empty, we will assign top_node to dst and Null terminate
+//  If not, top_node will point to the first node of dst, 
+//  as top_node -> [top_node1] -> [dst1] -> [dst2] -> NULL
+//  then we will make sure in this double linked list, next and prev
+//  are pointing to the right node, so the prev of top_node->next will not be NULL
+//  Then we will assign top_node to dst, so the node in top_node will be
+//  on the top of dst
+
+void	push_a(t_stack_node **a, t_stack_node **b, bool print)
+{
+	push(a, b);
+	if (!print)
+		ft_printf("pa\n");
+}
+//* EXPLANATION:
+//? This function is pushing the top element of a to the top of b
+
+void	push_b(t_stack_node **a, t_stack_node **b, bool print)
+{
+	push(b, a);
+	if (!print)
+		ft_printf("pb\n");
+}
+//* EXPLANATION:
+//? This function is pushing the top element of b to the top of a
---
+++ push_swap/src/cmd/rev_rotate.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_rotate.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 20:55:22 by jilin             #+#    #+#             */
+/*   Updated: 2025/02/19 19:21:26 by jilin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/push_swap.h"
+
+static void	rev_rotate(t_stack_node **stack)
+{
+	t_stack_node	*last_node;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	last_node = find_last(*stack);
+	last_node->prev->next = NULL;
+	last_node->next = *stack;
+	last_node->prev = NULL;
+	*stack = last_node;
+	last->next->prev = last;
+}
+//* EXPLANATION:
+//?	This function is rotating the stack's bottom node to the top
+//  If the stack is empty or has only one node, nothing to rotate
+//  Set last_node to the last node of the stack
+//  when we set the last_node->prev->next to NULL, we are detaching the last node,
+//  the second last node will be the last node
+//  When we point the next to *stack, it point to the top node of the stack
+//  Last node-s prev will be NULL, because it is the top node now
+//  Then we set the stack to last_node, so the last node will be the top node
+//  Then we update the prev pointer of the previous first node to point to the
+//  last node, so the doubly linked list is maintained
