8862b8eda2484ce43f1f17627f1f9101cebf16a2
Jingyang Lin <jilin@fu-r4-p6.s19.be>
2025-02-19 16:26:15 +0100

    ADD

--- push_swap/cmd/rotate.c
+++ push_swap/cmd/rotate.c
@@ -6,8 +6,25 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 20:55:24 by jilin             #+#    #+#             */
-/*   Updated: 2025/02/18 20:55:41 by jilin            ###   ########.fr       */
+/*   Updated: 2025/02/19 16:23:56 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
+
+static void	rotate(t_stack_node **stack)
+{
+	t_stack_node	*last_node;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	last_node = find_last(*stack);
+	last_node->next = *stack;
+	*stack = *stack->next;
+	(*stack)->prev = NULL;
+	last_node->next->prev = last_node;
+	last_node->next->next = NULL;
+}
+//* EXPLANATION:
+//?	This function is rotating the stack's top node to the bottom
+//  
