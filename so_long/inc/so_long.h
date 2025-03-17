2f8179ce0f10e7e1da3e79a7afbef99295c65e88
Jingyang Lin <ginleamited@BIGMAC.local>
2025-03-17 17:58:44 +0100

    ADD

--- so_long/inc/so_long.h
+++ so_long/inc/so_long.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 17:47:29 by jilin             #+#    #+#             */
+/*   Updated: 2025/03/17 17:51:05 by jilin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
@@ -8,18 +20,18 @@
 # include "../mlx/mlx.h"
 
 # define TILE_SIZE 64
-#define ESC 65307 // X11 keycode for ESC
-#define W 119    // X11 keycode for 'w'
-#define A 97     // X11 keycode for 'a'
-#define S 115    // X11 keycode for 's'
-#define D 100    // X11 keycode for 'd'
+# define ESC 65307
+# define W 119
+# define A 97
+# define S 115
+# define D 100
 
 typedef struct s_enemy
 {
-    int x;
-    int y;
-    struct s_enemy *next;
-} t_enemy;
+	int				x;
+	int				y;
+	struct s_enemy	*next;
+}	t_enemy;
 
 typedef struct s_game
 {
@@ -70,7 +82,7 @@ int		load_images(t_game *game);
 void	render_game(t_game *game);
 void	display_moves(t_game *game);
 // Player Movement and Input
-void 	player_position(t_game *g);
+void	player_position(t_game *g);
 void	player_direction(t_game *game, int dx, int dy);
 void	move_player(t_game *game, int dx, int dy);
 int		key_press(int key, t_game *game);
