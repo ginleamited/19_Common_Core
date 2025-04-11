0bfd75b12eeebfee144a47af5350fb21d8e7fde4
Jingyang Lin <ginleamited@BIGMAC.local>
2025-04-11 23:58:40 +0200

    ADD

--- pipex/src/pipex_process.c
+++ pipex/src/pipex_process.c
@@ -6,7 +6,7 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 20:38:43 by jilin             #+#    #+#             */
-/*   Updated: 2025/04/11 22:35:21 by jilin            ###   ########.fr       */
+/*   Updated: 2025/04/11 23:56:13 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,28 +42,22 @@ void	ft_run(char **args, char *cmd, char **env)
 void	ft_first_child(int fd[2], int file1, char *cmd1, char **env)
 {
 	char	**args;
-	int		status;
 
 	close(fd[0]);
 	args = ft_split(cmd1, ' ');
 	if (dup2(file1, 0) == -1 || dup2(fd[1], 1) == -1)
 	{
-		perror("cmd1");
-		if (WIFEXITED(status))
-			exit(WEXITSTATUS(status));
-		return ;
+		perror("dup2");
+		exit(1);
 	}
 	close(file1);
 	close(fd[1]);
-	if (args[0][0] == '/')
+	if (args[0] && args[0][0] == '/')
 	{
 		execve(args[0], args, env);
-		ft_putstr_fd("Failed to execute: ", 2);
-		ft_putstr_fd(args[0], 2);
-		ft_putstr_fd("\n", 2);
+		perror("execve");
 		ft_free(args);
-		if (WIFEXITED(status))
-			exit(WEXITSTATUS(status));
+		exit(127);
 	}
 	ft_run(args, args[0], env);
 }
@@ -71,28 +65,22 @@ void	ft_first_child(int fd[2], int file1, char *cmd1, char **env)
 void	ft_second_child(int fd[2], int file2, char *cmd2, char **env)
 {
 	char	**args;
-	int		status;
 
 	close(fd[1]);
 	args = ft_split(cmd2, ' ');
 	if (dup2(fd[0], 0) == -1 || dup2(file2, 1) == -1)
 	{
-		perror("cmd2");
-		if (WIFEXITED(status))
-			exit(WEXITSTATUS(status));
-		return ;
+		perror("dup2");
+		exit(1);
 	}
 	close(fd[0]);
 	close(file2);
-	if (args[0][0] == '/')
+	if (args[0] && args[0][0] == '/')
 	{
 		execve(args[0], args, env);
-		ft_putstr_fd("Failed to execute: ", 2);
-		ft_putstr_fd(args[0], 2);
-		ft_putstr_fd("\n", 2);
+		perror("execve");
 		ft_free(args);
-		if (WIFEXITED(status))
-			exit(WEXITSTATUS(status));
+		exit(127);
 	}
 	ft_run(args, args[0], env);
 }
