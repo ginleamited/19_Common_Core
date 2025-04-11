00eb99b52cab67cdd28be8f19220beca1e20c9f5
Jingyang Lin <ginleamited@BIGMAC.local>
2025-04-12 00:08:03 +0200

    ADD

--- pipex/src/pipex_process.c
+++ pipex/src/pipex_process.c
@@ -6,7 +6,7 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 20:38:43 by jilin             #+#    #+#             */
-/*   Updated: 2025/04/12 00:05:25 by jilin            ###   ########.fr       */
+/*   Updated: 2025/04/12 00:07:58 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,8 @@ void	ft_run(char **args, char *cmd, char **env)
 
 void ft_first_child(int fd[2], int file1, char *cmd, char **env)
 {
+    char **args;
+    
     // Close read end of pipe
     close(fd[0]);
     
@@ -62,32 +64,57 @@ void ft_first_child(int fd[2], int file1, char *cmd, char **env)
     if (!cmd || cmd[0] == '\0')
         exit(0);
     
-    // Otherwise try to execute it
-    execute_cmd(cmd, env);
+    // Use your existing command execution logic
+    args = ft_split(cmd, ' ');
+    if (!args)
+        exit(1);
     
-    // If execution fails, exit with status 127
+    if (args[0] && args[0][0] == '/')
+    {
+        execve(args[0], args, env);
+        perror("execve");
+        ft_free(args);
+        exit(127);
+    }
+    ft_run(args, args[0], env);
+    
+    // This line will only be reached if ft_run fails
+    ft_free(args);
     exit(127);
 }
 
-void	ft_second_child(int fd[2], int file2, char *cmd2, char **env)
+void ft_second_child(int fd[2], int file2, char *cmd2, char **env)
 {
-	char	**args;
+    char **args;
 
-	close(fd[1]);
-	args = ft_split(cmd2, ' ');
-	if (dup2(fd[0], 0) == -1 || dup2(file2, 1) == -1)
-	{
-		perror("dup2");
-		exit(1);
-	}
-	close(fd[0]);
-	close(file2);
-	if (args[0] && args[0][0] == '/')
-	{
-		execve(args[0], args, env);
-		perror("execve");
-		ft_free(args);
-		exit(127);
-	}
-	ft_run(args, args[0], env);
+    close(fd[1]);
+    
+    // Execute command (if empty, exit with status 0)
+    if (!cmd2 || cmd2[0] == '\0')
+        exit(0);
+    
+    if (dup2(fd[0], 0) == -1 || dup2(file2, 1) == -1)
+    {
+        perror("dup2");
+        exit(1);
+    }
+    close(fd[0]);
+    close(file2);
+    
+    args = ft_split(cmd2, ' ');
+    if (!args)
+        exit(1);
+    
+    if (args[0] && args[0][0] == '/')
+    {
+        execve(args[0], args, env);
+        perror("execve");
+        ft_free(args);
+        exit(127);
+    }
+    ft_run(args, args[0], env);
+    
+    // This line will only be reached if ft_run fails
+    ft_free(args);
+    exit(127);
 }
