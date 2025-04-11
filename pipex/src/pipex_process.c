2ff4c54c02c63ff5f15224e9e8099a9a2b25bc8b
Jingyang Lin <ginleamited@BIGMAC.local>
2025-04-12 00:05:33 +0200

    ADD

--- pipex/src/pipex_process.c
+++ pipex/src/pipex_process.c
@@ -6,7 +6,7 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 20:38:43 by jilin             #+#    #+#             */
-/*   Updated: 2025/04/11 23:56:13 by jilin            ###   ########.fr       */
+/*   Updated: 2025/04/12 00:05:25 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,27 +39,34 @@ void	ft_run(char **args, char *cmd, char **env)
 	exit(127);
 }
 
-void	ft_first_child(int fd[2], int file1, char *cmd1, char **env)
+void ft_first_child(int fd[2], int file1, char *cmd, char **env)
 {
-	char	**args;
-
-	close(fd[0]);
-	args = ft_split(cmd1, ' ');
-	if (dup2(file1, 0) == -1 || dup2(fd[1], 1) == -1)
-	{
-		perror("dup2");
-		exit(1);
-	}
-	close(file1);
-	close(fd[1]);
-	if (args[0] && args[0][0] == '/')
-	{
-		execve(args[0], args, env);
-		perror("execve");
-		ft_free(args);
-		exit(127);
-	}
-	ft_run(args, args[0], env);
+    // Close read end of pipe
+    close(fd[0]);
+    
+    // If file1 is valid, redirect stdin to file1
+    if (file1 >= 0)
+    {
+        if (dup2(file1, STDIN_FILENO) < 0)
+            exit(1);
+        close(file1);
+    }
+    // If file1 is invalid, stdin remains /dev/null
+    
+    // Redirect stdout to pipe write end
+    if (dup2(fd[1], STDOUT_FILENO) < 0)
+        exit(1);
+    close(fd[1]);
+    
+    // Execute command (if empty, exit with status 0)
+    if (!cmd || cmd[0] == '\0')
+        exit(0);
+    
+    // Otherwise try to execute it
+    execute_cmd(cmd, env);
+    
+    // If execution fails, exit with status 127
+    exit(127);
 }
 
 void	ft_second_child(int fd[2], int file2, char *cmd2, char **env)
