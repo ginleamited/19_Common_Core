9bacf3b0ca22f6ccbc623cb91c48b0657fba1df2
Jingyang Lin <ginleamited@BIGMAC.local>
2025-04-29 16:31:18 +0200

    AD

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,14 +6,27 @@
 /*   By: jilin <jilin@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 20:40:40 by jilin             #+#    #+#             */
-/*   Updated: 2025/04/17 20:41:20 by jilin            ###   ########.fr       */
+/*   Updated: 2025/04/25 16:52:47 by jilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-
+s_struct s_philo
+{
+	int				id;
+	int				philo_count;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				eat_count;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*print_mutex;
+	pthread_mutex_t	*eat_mutex;
+	pthread_mutex_t	*die_mutex;
+	pthread_t		thread_id;
+}	t_philo;
 
 
 
